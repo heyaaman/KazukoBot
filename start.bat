81f638c2f4d504c95bb72a9bf4203837b04597f0
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-20 20:57:05 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 TITLE Rui Bot
-:: Enables virtual env mode and then starts Rika
-env\scripts\activate.bat && py -m RuiBot
+:: Enables virtual env mode and then starts Kazuko
+env\scripts\activate.bat && py -m Kazukobot
