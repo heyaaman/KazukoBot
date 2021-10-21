e53a1b4a467a86191641a6ce8485204f24d2d297
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-21 12:01:07 +0530

    Update msg_types.py

--- KazukoBot/modules/helper_funcs/msg_types.py
+++ KazukoBot/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from YoneRobot.modules.helper_funcs.string_handling import button_markdown_parser
+from Kazukobot.modules.helper_funcs.string_handling import button_markdown_parser
 from telegram import Message
 
 
