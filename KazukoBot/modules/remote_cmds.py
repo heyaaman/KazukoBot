6c1c98a8c10bea4c210d516441ca00d72817b67b
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-21 17:16:47 +0530

    Update remote_cmds.py

--- KazukoBot/modules/remote_cmds.py
+++ KazukoBot/modules/remote_cmds.py
@@ -1,12 +1,12 @@
-from Kazukobot import dispatcher
-from Kazukobot.modules.helper_funcs.chat_status import (
+from KazukoBot import dispatcher
+from KazukoBot.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from Kazukobot.modules.helper_funcs.extraction import extract_user_and_text
-from Kazukobot.modules.helper_funcs.filters import CustomFilters
+from KazukoBot.modules.helper_funcs.extraction import extract_user_and_text
+from KazukoBot.modules.helper_funcs.filters import CustomFilters
 from telegram import Update, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
