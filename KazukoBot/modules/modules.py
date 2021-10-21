cca6796a5f583f7b4c357d3717622c8e406a9706
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-21 17:14:11 +0530

    Update modules.py

--- KazukoBot/modules/modules.py
+++ KazukoBot/modules/modules.py
@@ -1,8 +1,8 @@
 import importlib
 import collections
 
-from Kazukobot import dispatcher, telethn
-from Kazukobot.__main__ import (
+from KazukoBot import dispatcher, telethn
+from KazukoBot.__main__ import (
     CHAT_SETTINGS,
     DATA_EXPORT,
     DATA_IMPORT,
@@ -13,7 +13,7 @@ from Kazukobot.__main__ import (
     USER_INFO,
     USER_SETTINGS,
 )
-from Kazukobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from KazukoBot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
