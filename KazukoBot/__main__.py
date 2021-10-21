1489fcb1630ee3a583130b4f88eb51a81b2bb512
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-21 23:48:41 +0530

    Update __main__.py

--- KazukoBot/__main__.py
+++ KazukoBot/__main__.py
@@ -744,9 +744,7 @@ def main():
     updater.idle()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
     telethn.start(bot_token=TOKEN)
-    pgram.start()
     main()
-    idle()
