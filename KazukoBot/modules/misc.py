8a2b93c1bd8f873cf84d2d646f3197fab48e34f4
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-21 21:51:30 +0530

    Update misc.py

--- KazukoBot/modules/misc.py
+++ KazukoBot/modules/misc.py
@@ -10,16 +10,16 @@ from telegram.ext import CallbackContext, Filters, CommandHandler
 MARKDOWN_HELP = f"""
 Markdown is a very powerful formatting tool supported by telegram. {dispatcher.bot.first_name} has some enhancements, to make sure that \
 saved messages are correctly parsed, and to allow you to create buttons.
-• <code>_italic_</code>: wrapping text with '_' will produce italic text
-• <code>*bold*</code>: wrapping text with '*' will produce bold text
-• <code>`code`</code>: wrapping text with '`' will produce monospaced text, also known as 'code'
-• <code>[sometext](someURL)</code>: this will create a link - the message will just show <code>sometext</code>, \
+- <code>_italic_</code>: wrapping text with '_' will produce italic text
+- <code>*bold*</code>: wrapping text with '*' will produce bold text
+- <code>`code`</code>: wrapping text with '`' will produce monospaced text, also known as 'code'
+- <code>[sometext](someURL)</code>: this will create a link - the message will just show <code>sometext</code>, \
 and tapping on it will open the page at <code>someURL</code>.
-<b>Example:</b><code>[test](example.com)</code>
-• <code>[buttontext](buttonurl:someURL)</code>: this is a special enhancement to allow users to have telegram \
+EG: <code>[test](example.com)</code>
+- <code>[buttontext](buttonurl:someURL)</code>: this is a special enhancement to allow users to have telegram \
 buttons in their markdown. <code>buttontext</code> will be what is displayed on the button, and <code>someurl</code> \
 will be the url which is opened.
-<b>Example:</b> <code>[This is a button](buttonurl:example.com)</code>
+EG: <code>[This is a button](buttonurl:example.com)</code>
 If you want multiple buttons on the same line, use :same, as such:
 <code>[one](buttonurl://example.com)
 [two](buttonurl://google.com:same)</code>
@@ -27,92 +27,330 @@ This will create two buttons on a single line, instead of one button per line.
 Keep in mind that your message <b>MUST</b> contain some text other than just a button!
 """
 
-
 @run_async
 @user_admin
-def echo(update: Update, context: CallbackContext):
+def get_id(update: Update, context: CallbackContext):
+    bot, args = context.bot, context.args
+    message = update.effective_message
+    chat = update.effective_chat
+    msg = update.effective_message
+    user_id = extract_user(msg, args)
+
+    if user_id:
+
+        if msg.reply_to_message and msg.reply_to_message.forward_from:
+
+            user1 = message.reply_to_message.from_user
+            user2 = message.reply_to_message.forward_from
+
+            msg.reply_text(
+                f"<b>Telegram ID:</b>,"
+                f"• {html.escape(user2.first_name)} - <code>{user2.id}</code>.\n"
+                f"• {html.escape(user1.first_name)} - <code>{user1.id}</code>.",
+                parse_mode=ParseMode.HTML,
+            )
+
+        else:
+
+            user = bot.get_chat(user_id)
+            msg.reply_text(
+                f"{html.escape(user.first_name)}'s id is <code>{user.id}</code>.",
+                parse_mode=ParseMode.HTML,
+            )
+
+    else:
+
+        if chat.type == "private":
+            msg.reply_text(
+                f"Your id is <code>{chat.id}</code>.", parse_mode=ParseMode.HTML
+            )
+
+        else:
+            msg.reply_text(
+                f"This group's id is <code>{chat.id}</code>.", parse_mode=ParseMode.HTML
+            )
+
+@run_async
+def gifid(update: Update, _):
+    msg = update.effective_message
+    if msg.reply_to_message and msg.reply_to_message.animation:
+        update.effective_message.reply_text(
+            f"Gif ID:\n<code>{msg.reply_to_message.animation.file_id}</code>",
+            parse_mode=ParseMode.HTML,
+        )
+    else:
+        update.effective_message.reply_text("Please reply to a gif to get its ID.")
+
+@run_async
+def info(update: Update, context: CallbackContext):  # sourcery no-metrics
+    bot = context.bot
+    args = context.args
+    message = update.effective_message
+    chat = update.effective_chat
+    user_id = extract_user(update.effective_message, args)
+
+    if user_id:
+        user = bot.get_chat(user_id)
+
+    elif not message.reply_to_message and not args:
+        user = message.from_user
+
+    elif not message.reply_to_message and (
+        not args
+        or (
+            len(args) >= 1
+            and not args[0].startswith("@")
+            and not args[0].isdigit()
+            and not message.parse_entities([MessageEntity.TEXT_MENTION])
+        )
+    ):
+        message.reply_text("I can't extract a user from this.")
+        return
+
+    else:
+        return
+
+    text = (
+        f"<b>General:</b>\n"
+        f"ID: <code>{user.id}</code>\n"
+        f"First Name: {html.escape(user.first_name)}"
+    )
+
+    if user.last_name:
+        text += f"\nLast Name: {html.escape(user.last_name)}"
+
+    if user.username:
+        text += f"\nUsername: @{html.escape(user.username)}"
+
+    text += f"\nPermanent user link: {mention_html(user.id, 'link')}"
+
+    try:
+        spamwtc = sw.get_ban(int(user.id))
+        if spamwtc:
+            text += "<b>\n\nSpamWatch:\n</b>"
+            text += "<b>This person is banned in Spamwatch!</b>"
+            text += f"\nReason: <pre>{spamwtc.reason}</pre>"
+            text += "\nAppeal at @SpamWatchSupport"
+        else:
+            text += "<b>\n\nSpamWatch:</b>\n Not banned"
+    except:
+        pass  # don't crash if api is down somehow...
+
+    Nation_level_present = False
+
+    num_chats = sql.get_user_num_chats(user.id)
+    text += f"\n<b>Chat count</b>: <code>{num_chats}</code>"
+
+    try:
+        user_member = chat.get_member(user.id)
+        if user_member.status == "administrator":
+            result = bot.get_chat_member(chat.id, user.id).to_json()
+            result = result["result"]
+            if "custom_title" in result.keys():
+                custom_title = result["custom_title"]
+                text += f"\nThis user holds the title <b>{custom_title}</b> here."
+    except BadRequest:
+        pass
+
+
+    if user.id == OWNER_ID:
+        text += '\nThis person is my owner'
+        Nation_level_present = True
+    elif user.id in DEV_USERS:
+        text += '\nThis Person is a part of Eagle Union'
+        Nation_level_present = True
+    elif user.id in SUDO_USERS:
+        text += '\nThe Nation level of this person is Royal'
+        Nation_level_present = True
+    elif user.id in SUPPORT_USERS:
+        text += '\nThe Nation level of this person is Sakura'
+        Nation_level_present = True
+    elif user.id in SARDEGNA_USERS:
+        text += '\nThe Nation level of this person is Sardegna'
+        Nation_level_present = True
+    elif user.id in WHITELIST_USERS:
+        text += '\nThe Nation level of this person is Neptunia'
+        Nation_level_present = True
+
+    if Nation_level_present:
+        text += ' [<a href="https://t.me/{}?start=nations">?</a>]'.format(bot.username)
+
+    text += "\n"
+    for mod in USER_INFO:
+        if mod.__mod_name__ == "Users":
+            continue
+
+        try:
+            mod_info = mod.__user_info__(user.id)
+        except TypeError:
+            mod_info = mod.__user_info__(user.id, chat.id)
+        if mod_info:
+            text += "\n" + mod_info
+
+    if INFOPIC:
+        try:
+            profile = bot.get_user_profile_photos(user.id).photos[0][-1]
+            _file = bot.get_file(profile["file_id"])
+
+            _file = _file.download(out=BytesIO())
+            _file.seek(0)
+
+            message.reply_document(
+                document=_file,
+                caption=(text),
+                parse_mode=ParseMode.HTML,
+            )
+
+        # Incase user don't have profile pic, send normal text
+        except IndexError:
+            message.reply_text(
+                text, parse_mode=ParseMode.HTML, disable_web_page_preview=True
+            )
+
+    else:
+        message.reply_text(
+            text, parse_mode=ParseMode.HTML, disable_web_page_preview=True
+        )
+
+@run_async(command='echo', pass_args=True, filters=Filters.chat_type.groups)
+@user_admin
+def echo(update: Update, _):
     args = update.effective_message.text.split(None, 1)
     message = update.effective_message
 
     if message.reply_to_message:
-        message.reply_to_message.reply_text(
-            args[1], parse_mode="MARKDOWN", disable_web_page_preview=True,
-        )
+        message.reply_to_message.reply_text(args[1])
     else:
-        message.reply_text(
-            args[1], quote=False, parse_mode="MARKDOWN", disable_web_page_preview=True,
-        )
+        message.reply_text(args[1], quote=False)
+
     message.delete()
 
 
-def markdown_help_sender(update: Update):
-    update.effective_message.reply_text(MARKDOWN_HELP, parse_mode=ParseMode.HTML)
+def shell(command):
+    process = Popen(command, stdout=PIPE, shell=True, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    return (stdout, stderr)
+
+@run_async(command='markdownhelp', filters=Filters.chat_type.private)
+def markdown_help(update: Update, _):
+    chat = update.effective_chat
+    update.effective_message.reply_text((gs(chat.id, "markdown_help_text")), parse_mode=ParseMode.HTML)
     update.effective_message.reply_text(
-        "Try forwarding the following message to me, and you'll see, and Use #test!",
+        "Try forwarding the following message to me, and you'll see!"
     )
     update.effective_message.reply_text(
-        "/save test This is a markdown test. _italics_, *bold*, code, "
+        "/save test This is a markdown test. _italics_, *bold*, `code`, "
         "[URL](example.com) [button](buttonurl:github.com) "
-        "[button2](buttonurl://google.com:same)",
+        "[button2](buttonurl://google.com:same)"
+    )
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
+stats_str = '''
+'''
+@run_async(command='stats', can_disable=False)
+@sudo_plus
+def stats(update, context):
+    db_size = SESSION.execute("SELECT pg_size_pretty(pg_database_size(current_database()))").scalar_one_or_none()
+    uptime = datetime.datetime.fromtimestamp(boot_time()).strftime("%Y-%m-%d %H:%M:%S")
+    botuptime = get_readable_time((time.time() - StartTime))
+    status = "*╒═══「 System statistics: 」*\n\n"
+    status += "*• System Start time:* " + str(uptime) + "\n"
+    uname = platform.uname()
+    status += "*• System:* " + str(uname.system) + "\n"
+    status += "*• Node name:* " + escape_markdown(str(uname.node)) + "\n"
+    status += "*• Release:* " + escape_markdown(str(uname.release)) + "\n"
+    status += "*• Machine:* " + escape_markdown(str(uname.machine)) + "\n"
+
+    mem = virtual_memory()
+    cpu = cpu_percent()
+    disk = disk_usage("/")
+    status += "*• CPU:* " + str(cpu) + " %\n"
+    status += "*• RAM:* " + str(mem[2]) + " %\n"
+    status += "*• Storage:* " + str(disk[3]) + " %\n\n"
+    status += "*• Python version:* " + python_version() + "\n"
+    status += "*• python-telegram-bot:* " + str(ptbver) + "\n"
+    status += "*• Uptime:* " + str(botuptime) + "\n"
+    status += "*• Database size:* " + str(db_size) + "\n"
+    kb = [
+          [
+           InlineKeyboardButton('Ping', callback_data='pingCB')
+          ]
+    ]
+    repo = git.Repo(search_parent_directories=True)
+    sha = repo.head.object.hexsha
+    status += f"*• Commit*: `{sha[0:9]}`\n"
+    try:
+        update.effective_message.reply_text(status +
+            "\n*Bot statistics*:\n"
+            + "\n".join([mod.__stats__() for mod in STATS]) +
+            "\n\n[⍙ GitHub](https://github.com/heyaaman/KazukoBot) | [Support](https://t.me/CFC_BOT_support)\n\n" +
+            "╘══「 by [heyaaman](github.com/heyaaman) 」\n",
+        parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(kb), disable_web_page_preview=True)
+    except BaseException:
+        update.effective_message.reply_text(
+            (
+                (
+                    (
+                        "\n*Bot statistics*:\n"
+                        + "\n".join(mod.__stats__() for mod in STATS)
+                    )
+                    + "\n\n⍙ [GitHub](https://github.com/heyaaman/KazukoBot) | ⍚ [Support](https://t.me/CFC_BOT_support)\n\n"
+                )
+                + "╘══「 by [heyaaman](github.com/heyaaman) 」\n"
+            ),
+            parse_mode=ParseMode.MARKDOWN,
+            reply_markup=InlineKeyboardMarkup(kb),
+            disable_web_page_preview=True,
+        )
+
+@run_async(command='ping')
+def ping(update: Update, _):
+    msg = update.effective_message
+    start_time = time.time()
+    message = msg.reply_text("Pinging...")
+    end_time = time.time()
+    ping_time = round((end_time - start_time) * 1000, 3)
+    message.edit_text(
+        "*Pong!!!*\n`{}ms`".format(ping_time), parse_mode=ParseMode.MARKDOWN
     )
 
 
 @run_async
-def markdown_help(update: Update, context: CallbackContext):
-    if update.effective_chat.type != "private":
-        update.effective_message.reply_text(
-            "Contact me in pm",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            "Markdown help",
-                            url=f"t.me/{context.bot.username}?start=markdownhelp",
-                        ),
-                    ],
-                ],
-            ),
-        )
-        return
-    markdown_help_sender(update)
+def pingCallback(update: Update, context: CallbackContext):
+    query = update.callback_query
+    start_time = time.time()
+    requests.get('https://api.telegram.org')
+    end_time = time.time()
+    ping_time = round((end_time - start_time) * 1000, 3)
+    query.answer('Pong! {}ms'.format(ping_time))
 
 
-__help__ = """
-*Available commands:*
-*Markdown:*
- • `/markdownhelp`*:* quick summary of how markdown works in telegram - can only be called in private chats
-*Paste:*
- • `/paste`*:* Saves replied content to `nekobin.com` and replies with a url
-*React:*
- • `/react`*:* Reacts with a random reaction
-*Urban Dictonary:*
- • `/ud <word>`*:* Type the word or expression you want to search use
-*Wikipedia:*
- • `/wiki <query>`*:* wikipedia your query
-*Wallpapers:*
- • `/wall <query>`*:* get a wallpaper from wall.alphacoders.com
-*Currency converter:*
- • `/cash`*:* currency converter
-Example:
- `/cash 1 USD INR`
-      _OR_
- `/cash 1 usd inr`
-Output: `1.0 USD = 75.505 INR`
-*Timezones:*
- • `/time <query>`*:* Gives information about a timezone.
-*Available queries:* Country Code/Country Name/Timezone Name
-• 🕐 [Timezones list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
-"""
+def get_help(chat):
+    return gs(chat, "misc_help")
 
-ECHO_HANDLER = DisableAbleCommandHandler("echo", echo, filters=Filters.group)
-MD_HELP_HANDLER = CommandHandler("markdownhelp", markdown_help)
 
-dispatcher.add_handler(ECHO_HANDLER)
-dispatcher.add_handler(MD_HELP_HANDLER)
 
-__mod_name__ = "Extras"
-__command_list__ = ["id", "echo"]
-__handlers__ = [
-    ECHO_HANDLER,
-    MD_HELP_HANDLER,
-]
+__mod_name__ = "Misc"
