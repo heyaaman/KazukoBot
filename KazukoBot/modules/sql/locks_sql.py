a2925aa690f279c24edb3fb1738afb2d432fb36f
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-21 16:46:35 +0530

    Update locks_sql.py

--- KazukoBot/modules/sql/locks_sql.py
+++ KazukoBot/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from Kazukobot.modules.sql import SESSION, BASE
+from KazukoBot.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
