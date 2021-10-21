d1c5b2245bf22ec4a0e98bfb36f7f037236e04af
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-21 18:29:41 +0530

    Update log_channel.py

--- KazukoBot/modules/log_channel.py
+++ KazukoBot/modules/log_channel.py
@@ -3,7 +3,7 @@ from functools import wraps
 
 from telegram.ext import CallbackContext
 
-from Kazukobot.modules.helper_funcs.misc import is_module_loaded
+from KazukoBot.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
@@ -13,9 +13,9 @@ if is_module_loaded(FILENAME):
     from telegram.ext import CommandHandler, JobQueue, run_async
     from telegram.utils.helpers import escape_markdown
 
-    from Kazukobot import EVENT_LOGS, LOGGER, dispatcher
-    from Kazukobot.modules.helper_funcs.chat_status import user_admin
-    from Kazukobot.modules.sql import log_channel_sql as sql
+    from KazukoBot import EVENT_LOGS, LOGGER, dispatcher
+    from KazukoBot.modules.helper_funcs.chat_status import user_admin
+    from KazukoBot.modules.sql import log_channel_sql as sql
 
     def loggable(func):
         @wraps(func)
