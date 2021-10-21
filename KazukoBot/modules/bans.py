12eb44168eee4c96fcba1e9bc884a2d736161825
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-21 17:00:23 +0530

    Update bans.py

--- KazukoBot/modules/bans.py
+++ KazukoBot/modules/bans.py
@@ -5,7 +5,7 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from Kazukobot import (
+from KazukoBot import (
     DEV_USERS,
     LOGGER,
     OWNER_ID,
@@ -15,8 +15,8 @@ from Kazukobot import (
     WOLVES,
     dispatcher,
 )
-from Kazukobot.modules.disable import DisableAbleCommandHandler
-from Kazukobot.modules.helper_funcs.chat_status import (
+from KazukoBot.modules.disable import DisableAbleCommandHandler
+from KazukoBot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     connection_status,
@@ -27,9 +27,9 @@ from Kazukobot.modules.helper_funcs.chat_status import (
     user_can_ban,
     can_delete,
 )
-from Kazukobot.modules.helper_funcs.extraction import extract_user_and_text
-from Kazukobot.modules.helper_funcs.string_handling import extract_time
-from Kazukobot.modules.log_channel import gloggable, loggable
+from KazukoBot.modules.helper_funcs.extraction import extract_user_and_text
+from KazukoBot.modules.helper_funcs.string_handling import extract_time
+from KazukoBot.modules.log_channel import gloggable, loggable
 
 
 @run_async
