4cf360c708436ed3679a484b75e1cef7ebccf92e
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-21 17:05:47 +0530

    Update dev.py

--- KazukoBot/modules/dev.py
+++ KazukoBot/modules/dev.py
@@ -5,10 +5,10 @@ import sys
 from contextlib import suppress
 from time import sleep
 
-import Kazukobot
+import KazukoBot
 
-from Kazukobot import dispatcher
-from Kazukobot.modules.helper_funcs.chat_status import dev_plus
+from KazukoBot import dispatcher
+from KazukoBot.modules.helper_funcs.chat_status import dev_plus
 from telegram import TelegramError, Update
 from telegram.error import Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, run_async
