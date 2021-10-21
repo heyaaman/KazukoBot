9a1cd13d17efce6a1a4e9d8feae22bca95e4de82
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-21 16:43:42 +0530

    Update blacklist_sql.py

--- KazukoBot/modules/sql/blacklist_sql.py
+++ KazukoBot/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from Kazukobot.modules.sql import SESSION, BASE
+from KazukoBot.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
