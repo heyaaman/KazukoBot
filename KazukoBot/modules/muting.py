aa21234f20a7b44d6b2ce34117212afd61b95b3a
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-21 17:14:41 +0530

    Update muting.py

--- KazukoBot/modules/muting.py
+++ KazukoBot/modules/muting.py
@@ -1,20 +1,20 @@
 import html
 from typing import Optional
 
-from Kazukobot import LOGGER, TIGERS, dispatcher
-from Kazukobot.modules.helper_funcs.chat_status import (
+from KazukoBot import LOGGER, TIGERS, dispatcher
+from KazukoBot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     connection_status,
     is_user_admin,
     user_admin,
 )
-from Kazukobot.modules.helper_funcs.extraction import (
+from KazukoBot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from Kazukobot.modules.helper_funcs.string_handling import extract_time
-from Kazukobot.modules.log_channel import loggable
+from KazukoBot.modules.helper_funcs.string_handling import extract_time
+from KazukoBot.modules.log_channel import loggable
 from telegram import Bot, Chat, ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
