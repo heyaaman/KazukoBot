f1b07316fbe37b86f79a3c38ae07632c8b5148b1
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-21 17:07:19 +0530

    Update error_handler.py

--- KazukoBot/modules/error_handler.py
+++ KazukoBot/modules/error_handler.py
@@ -8,7 +8,7 @@ import pretty_errors
 import io
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CallbackContext, CommandHandler
-from Kazukobot import dispatcher, DEV_USERS, OWNER_ID
+from KazukoBot import dispatcher, DEV_USERS, OWNER_ID
 
 pretty_errors.mono()
 
