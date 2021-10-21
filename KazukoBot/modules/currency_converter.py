4ba3d102aff5325560086c3d95b8805df831efb9
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-21 17:03:22 +0530

    Update currency_converter.py

--- KazukoBot/modules/currency_converter.py
+++ KazukoBot/modules/currency_converter.py
@@ -1,5 +1,5 @@
 import requests
-from Kazukobot import CASH_API_KEY, dispatcher
+from KazukoBot import CASH_API_KEY, dispatcher
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
