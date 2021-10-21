45d8c74d9f971dc87dabbc201725ffa23874e70f
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-21 16:42:17 +0530

    Update __init__.py

--- KazukoBot/modules/sql/__init__.py
+++ KazukoBot/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from Kazukobot import DB_URI
+from KazukoBot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
