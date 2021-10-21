e4008926b96aaa01c72ed19e8fec5a551bb35019
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-21 18:18:39 +0530

    Update __main__.py

--- KazukoBot/__main__.py
+++ KazukoBot/__main__.py
@@ -111,7 +111,7 @@ CHAT_SETTINGS = {}
 USER_SETTINGS = {}
 
 for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("Kaziko.modules." + module_name)
+    imported_module = importlib.import_module("KazukoBot.modules." + module_name)
     if not hasattr(imported_module, "__mod_name__"):
         imported_module.__mod_name__ = imported_module.__name__
 
