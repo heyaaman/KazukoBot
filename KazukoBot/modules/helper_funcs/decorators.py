17fec01bb6fc962a349715587b1cb4083758fa0c
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-24 13:20:08 +0530

    Update decorators.py

--- KazukoBot/modules/helper_funcs/decorators.py
+++ KazukoBot/modules/helper_funcs/decorators.py
@@ -72,7 +72,7 @@ class KigyoTelegramHandler:
             return func
         return _callbackquery
 
-    def inlinequery(self, pattern: str = None, run_async: bool = True, pass_user_data: bool = True, pass_chat_data: bool = True, chat_types: List[str] = None):
+    def inlinequery(self, pattern: str = None, run_async: bool = True, pass_user_data: bool = True, pass_chat_data: bool = True, chat_types: str = None):
         def _inlinequery(func):
             self._dispatcher.add_handler(InlineQueryHandler(pattern=pattern, callback=func, run_async=run_async, pass_user_data=pass_user_data, pass_chat_data=pass_chat_data, chat_types=chat_types))
             log.debug(f'[KIGINLINE] Loaded inlinequery handler with pattern {pattern} for function {func.__name__} | PASSES USER DATA: {pass_user_data} | PASSES CHAT DATA: {pass_chat_data} | CHAT TYPES: {chat_types}')
