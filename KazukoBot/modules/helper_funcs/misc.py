4a1236c967034d97b58775f97fd9c56fdbe50542
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-24 12:41:54 +0530

    Update misc.py

--- KazukoBot/modules/helper_funcs/misc.py
+++ KazukoBot/modules/helper_funcs/misc.py
@@ -1,7 +1,7 @@
 from typing import Dict, List
 
 from KazukoBot import NO_LOAD
-from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton,InlineKeyboardMarkup, ParseMode
+from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton,InlineKeyboardMarkup, ParseMode, InlineQueryResultArticle, InputTextMessageContent
 from telegram.error import TelegramError
 
 
