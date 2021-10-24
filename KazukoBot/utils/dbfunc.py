323a16f8efdad1284513e3dc1467a72479203e9c
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-24 14:46:59 +0530

    Update dbfunc.py

--- KazukoBot/utils/dbfunc.py
+++ KazukoBot/utils/dbfunc.py
@@ -38,56 +38,6 @@ async def save_couple(chat_id: int, date: str, couple: dict):
     )
 
 
-# Karma functions
-
-
-async def get_karmas_count() -> dict:
-    chats = karmadb.find({"chat_id": {"$lt": 0}})
-    if not chats:
-        return {}
-    chats_count = 0
-    karmas_count = 0
-    for chat in await chats.to_list(length=1000000):
-        for i in chat['karma']:
-            karmas_count += chat['karma'][i]['karma']
-        chats_count += 1
-    return {
-        "chats_count": chats_count,
-        "karmas_count": karmas_count
-    }
-
-
-async def get_karmas(chat_id: int) -> Dict[str, int]:
-    karma = karmadb.find_one({"chat_id": chat_id})
-    if karma:
-        karma = karma['karma']
-    else:
-        karma = {}
-    return karma
-
-
-async def get_karma(chat_id: int, name: str) -> Union[bool, dict]:
-    name = name.lower().strip()
-    karmas = await get_karmas(chat_id)
-    if name in karmas:
-        return karmas[name]
-
-
-async def update_karma(chat_id: int, name: str, karma: dict):
-    name = name.lower().strip()
-    karmas = await get_karmas(chat_id)
-    karmas[name] = karma
-    karmadb.update_one(
-        {"chat_id": chat_id},
-        {
-            "$set": {
-                "karma": karmas
-            }
-        },
-        upsert=True
-    )
-
-
 # Alpha integer 
 
 async def int_to_alpha(user_id: int) -> str:
