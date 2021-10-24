890003853bd6bc6376bea99c88b306e83005ea66
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-10-24 14:32:43 +0530

    Update inline.py

--- KazukoBot/modules/inline.py
+++ KazukoBot/modules/inline.py
@@ -31,7 +31,7 @@ def remove_prefix(text, prefix):
         text = text.replace(prefix, "", 1)
     return text
 
-@run_async 
+@kiginline  
 def inlinequery(update: Update, _) -> None:
     """
     Main InlineQueryHandler callback.
