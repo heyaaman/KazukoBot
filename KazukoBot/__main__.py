6541a018b3d0df790cadd4f249ae90e46a5c1a1e
おい aaman <heyaaman2004@gmail.com>
2021-10-26 12:46:15 +0530

    Update __main__.py

--- KazukoBot/__main__.py
+++ KazukoBot/__main__.py
@@ -437,7 +437,7 @@ def kazuko_tac_callback(update, context):
 ☉ NSFW will get permanent global ban in Kazuko which never removes, report spammers on Kazuko's support 
 
 NOTE: Terms and Conditions will be change anytime."""
-      
+            parse_mode=ParseMode.HTML,
             reply_markup=InlineKeyboardMarkup(
                 [
                  [
