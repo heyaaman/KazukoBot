fde56d23e0af829ef2846d9e7baf86f41a24cf02
おい aaman <heyaaman2004@gmail.com>
2021-11-01 13:58:56 +0530

    Update __main__.py

--- KazukoBot/__main__.py
+++ KazukoBot/__main__.py
@@ -380,7 +380,7 @@ or choosing help button at home menu and report error/bugs at Kazuko's support c
                 [
                     [
                         InlineKeyboardButton(
-                            text=" Setup Guide ", url="https://t.me/CFC_BotUpdates/109",
+                            text=" Setup Guide ", url="https://t.me/Phoenix_Empire/110",
                         ),
                         InlineKeyboardButton(text="T & C", url="https://telegra.ph/Terms-and-Conditions-10-26",
                         ),
