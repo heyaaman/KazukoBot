d188435ac522c73d59f5656939584f8b21bbc3ae
おい aaman <heyaaman2004@gmail.com>
2021-11-03 12:52:38 +0530

    Update kukiai.py

--- KazukoBot/modules/kukiai.py
+++ KazukoBot/modules/kukiai.py
@@ -2,7 +2,7 @@ import json
 import re
 import html
 import requests
-import mrjoker.modules.sql.kukiai_sql as sql
+import KazukoBot.modules.sql.kukiai_sql as sql
 
 from time import sleep
 from pyrogram.types import (
