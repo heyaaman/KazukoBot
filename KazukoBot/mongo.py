3f547d0d467017e1edf02f77f315102f8b7c1cbe
おい aaman <heyaaman2004@gmail.com>
2021-11-03 14:56:23 +0530

    Update mongo.py

--- KazukoBot/mongo.py
+++ KazukoBot/mongo.py
@@ -2,7 +2,7 @@ import asyncio
 import sys
 
 from motor import motor_asyncio
-from MashaRoBot import MONGO_DB_URI 
+from KazukoBot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 from KazukoBot.conf import get_int_key, get_str_key
