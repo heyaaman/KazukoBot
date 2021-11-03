950b9e12fc24140ac504b932c791087316704f14
おい aaman <heyaaman2004@gmail.com>
2021-11-03 15:27:47 +0530

    Update conf.py

--- KazukoBot/conf.py
+++ KazukoBot/conf.py
@@ -11,30 +11,11 @@ def get_str_key(name, required=False):
     else:
         default = None
     if not (data := env.str(name, default=default)) and not required:
-        log.warn("No str key: " + name)
-        return None
-    elif not data:
-        log.critical("No str key: " + name)
-        sys.exit(2)
-    else:
-        return data
-
-
-
-DEFAULTS = {
-    "LOAD_MODULES": True,
-}
-
-def get_str_key(name, required=False):
-    if name in DEFAULTS:
-        default = DEFAULTS[name]
-    else:
-        default = None
-    if not (data := env.str(name, default=default)) and not required:
         LOGGER.warn("No str key: " + name)
         return None
     elif not data:
         LOGGER.critical("No str key: " + name)
+        sys.exit(2)
     else:
         return data
 
@@ -48,5 +29,6 @@ def get_int_key(name, required=False):
         return None
     elif not data:
         LOGGER.critical("No int key: " + name)
+        sys.exit(2)
     else:
         return data
