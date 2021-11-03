510764a560900f447ff015e02800e1574490df45
おい aaman <heyaaman2004@gmail.com>
2021-11-03 15:13:40 +0530

    Update conf.py

--- KazukoBot/conf.py
+++ KazukoBot/conf.py
@@ -20,31 +20,30 @@ def get_str_key(name, required=False):
         return data
 
 
+def get_str_key(name, required=False):
+    if name in DEFAULTS:
+        default = DEFAULTS[name]
+    else:
+        default = None
+    if not (data := env.str(name, default=default)) and not required:
+        LOGGER.warn("No str key: " + name)
+        return None
+    elif not data:
+        LOGGER.critical("No str key: " + name)
+        sys.exit(2)
+    else:
+        return data
+
 def get_int_key(name, required=False):
     if name in DEFAULTS:
         default = DEFAULTS[name]
     else:
         default = None
     if not (data := env.int(name, default=default)) and not required:
-        log.warn("No int key: " + name)
+        LOGGER.warn("No int key: " + name)
         return None
     elif not data:
-        log.critical("No int key: " + name)
-        sys.exit(2)
-    else:
-        return data
-
-
-def get_list_key(name, required=False):
-    if name in DEFAULTS:
-        default = DEFAULTS[name]
-    else:
-        default = None
-    if not (data := env.list(name, default=default)) and not required:
-        log.warn("No list key: " + name)
-        return []
-    elif not data:
-        log.critical("No list key: " + name)
+        LOGGER.critical("No int key: " + name)
         sys.exit(2)
     else:
         return data
@@ -56,10 +55,10 @@ def get_bool_key(name, required=False):
     else:
         default = None
     if not (data := env.bool(name, default=default)) and not required:
-        log.warn("No bool key: " + name)
+        LOGGER.warn("No bool key: " + name)
         return False
     elif not data:
-        log.critical("No bool key: " + name)
+        LOGGER.critical("No bool key: " + name)
         sys.exit(2)
     else:
         return data
