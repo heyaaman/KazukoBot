3cc1ad3d23b4446812c819617a29cf180d8b3ba8
おい aaman <heyaaman2004@gmail.com>
2021-11-03 12:40:24 +0530

    Create chatbots.py

---
+++ KazukoBot/modules/chatbots.py
@@ -0,0 +1,8 @@
+__help__= f"""
+
+*Kazuko AI Chatbot* 
+🔹 /addkazuko : Enables and Disables kazuko AI Chat mode (EXCLUSIVE)
+*Powered by* : [Kuki Chatbot](https://github.com/MoeZilla/KukiChatbot)
+"""
+
+__mod_name__ = "ChatBots"
