6899c92f37ccaaeb74fd3ecc33c140ea055bade9
おい aaman <heyaaman2004@gmail.com>
2021-11-04 10:30:26 +0530

    Update __init__.py

--- KazukoBot/__init__.py
+++ KazukoBot/__init__.py
@@ -172,6 +172,7 @@ else:
     ALLOW_EXCL = Config.ALLOW_EXCL
     CASH_API_KEY = Config.CASH_API_KEY
     TIME_API_KEY = Config.TIME_API_KEY
+    ARQ_API_KEY = Config.ARQ_API_KEY
     AI_API_KEY = Config.AI_API_KEY
     WALL_API = Config.WALL_API
     SUPPORT_CHAT = Config.SUPPORT_CHAT
