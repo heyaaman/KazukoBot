8ea795bb54a42bd8f93ea56042a92efc4eb132b0
おい aaman <heyaaman2004@gmail.com>
2021-11-05 17:12:09 +0530

    Update misc.py

--- KazukoBot/modules/helper_funcs/misc.py
+++ KazukoBot/modules/helper_funcs/misc.py
@@ -1,9 +1,8 @@
 from typing import Dict, List
 
+from KazukoBot import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
-
-from KazukoBot import NO_LOAD
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
@@ -21,21 +20,20 @@ def split_message(msg: str) -> List[str]:
     if len(msg) < MAX_MESSAGE_LENGTH:
         return [msg]
 
-    else:
-        lines = msg.splitlines(True)
-        small_msg = ""
-        result = []
-        for line in lines:
-            if len(small_msg) + len(line) < MAX_MESSAGE_LENGTH:
-                small_msg += line
-            else:
-                result.append(small_msg)
-                small_msg = line
+    lines = msg.splitlines(True)
+    small_msg = ""
+    result = []
+    for line in lines:
+        if len(small_msg) + len(line) < MAX_MESSAGE_LENGTH:
+            small_msg += line
         else:
-            # Else statement at the end of the for loop, so append the leftover string.
             result.append(small_msg)
+            small_msg = line
+    else:
+        # Else statement at the end of the for loop, so append the leftover string.
+        result.append(small_msg)
 
-        return result
+    return result
 
 
 def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
@@ -68,7 +66,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
 
     round_num = len(modules) / 3
     calc = len(modules) - round(round_num)
-    if calc == 1:
+    if calc in [1, 2]:
         pairs.append((modules[-1],))
     elif calc == 2:
         pairs.append((modules[-1],))
@@ -133,4 +131,3 @@ def build_keyboard_parser(bot, chat_id, buttons):
 
 def is_module_loaded(name):
     return name not in NO_LOAD
-
