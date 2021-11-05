888c6c4d62d129cce979fa9cc48b2d5e8b648955
おい aaman <heyaaman2004@gmail.com>
2021-11-05 14:36:25 +0530

    Update tagall.py

--- KazukoBot/modules/tagall.py
+++ KazukoBot/modules/tagall.py
@@ -21,7 +21,7 @@ async def tagall(client, message):
         await client.send_message(message.chat.id, j, parse_mode="html")
 
 
-__mod_name__ = "Tagall"
+__mod_name__ = ""
 __help__ = """
 - /tagall : Tag everyone in a chat
 """
