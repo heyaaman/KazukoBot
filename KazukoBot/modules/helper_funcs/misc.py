0a2eeed4486e0005eaa89087800a48fb9bcdcae2
おい aaman <heyaaman2004@gmail.com>
2021-11-05 16:44:52 +0530

    Update misc.py

--- KazukoBot/modules/helper_funcs/misc.py
+++ KazukoBot/modules/helper_funcs/misc.py
@@ -1,8 +1,9 @@
 from typing import Dict, List
 
-from KazukoBot import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
+
+from KazukoBot import NO_LOAD
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
@@ -20,20 +21,21 @@ def split_message(msg: str) -> List[str]:
     if len(msg) < MAX_MESSAGE_LENGTH:
         return [msg]
 
-    lines = msg.splitlines(True)
-    small_msg = ""
-    result = []
-    for line in lines:
-        if len(small_msg) + len(line) < MAX_MESSAGE_LENGTH:
-            small_msg += line
-        else:
-            result.append(small_msg)
-            small_msg = line
     else:
-        # Else statement at the end of the for loop, so append the leftover string.
-        result.append(small_msg)
+        lines = msg.splitlines(True)
+        small_msg = ""
+        result = []
+        for line in lines:
+            if len(small_msg) + len(line) < MAX_MESSAGE_LENGTH:
+                small_msg += line
+            else:
+                result.append(small_msg)
+                small_msg = line
+        else:
+            # Else statement at the end of the for loop, so append the leftover string.
+            result.append(small_msg)
 
-    return result
+        return result
 
 
 def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
@@ -66,12 +68,13 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
 
     round_num = len(modules) / 3
     calc = len(modules) - round(round_num)
-    if calc in [1, 2]:
+    if calc == 1:
         pairs.append((modules[-1],))
-    return pairs
+    elif calc == 2:
+        pairs.append((modules[-1],))
 
     else:
-        pairs += [[EqInlineKeyboardButton("BACK", callback_data="kazuko_back")]]
+        pairs += [[EqInlineKeyboardButton("Back", callback_data="kazuko_back")]]
 
     return pairs
 
@@ -129,5 +132,5 @@ def build_keyboard_parser(bot, chat_id, buttons):
 
 
 def is_module_loaded(name):
-    return name not in NO_LOAD 
+    return name not in NO_LOAD
 
