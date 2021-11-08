724c066ffb6206ae1ba9644d4543e46423689be3
おい aaman <heyaaman2004@gmail.com>
2021-11-08 09:24:17 +0530

    Update karma.py

--- KazukoBot/modules/karma.py
+++ KazukoBot/modules/karma.py
@@ -12,8 +12,9 @@ from KazukoBot.utils.dbfunctions import (alpha_to_int, get_karma, get_karmas,
 from KazukoBot.utils.filter_groups import karma_negative_group, karma_positive_group
 from KazukoBot.utils.functions import get_user_id_and_usernames
 
-__MODULE__ = "Karma"
-__HELP__ = """[UPVOTE] - Use upvote keywords like "+", "+1", "thanks" etc to upvote a message.
+__mod_name__ = "Karma"
+__help__ = """
+[UPVOTE] - Use upvote keywords like "+", "+1", "thanks" etc to upvote a message.
 [DOWNVOTE] - Use downvote keywords like "-", "-1", etc to downvote a message.
 /karma_toggle [ENABLE|DISABLE] - Enable or Disable Karma System In Your Chat.
 Reply to a message with /karma to check a user's karma
