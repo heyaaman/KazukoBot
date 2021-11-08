347751e6e5ee0d34cd44e497397766b69e90c1f3
おい aaman <heyaaman2004@gmail.com>
2021-11-08 08:46:07 +0530

    Update admin.py

--- KazukoBot/modules/admin.py
+++ KazukoBot/modules/admin.py
@@ -29,6 +29,34 @@ from KazukoBot.modules.log_channel import loggable
 from KazukoBot.modules.helper_funcs.alternate import send_message
 from KazukoBot.modules.helper_funcs.alternate import typing_action
 
+async def member_permissions(chat_id: int, user_id: int):
+    perms = []
+    try:
+        member = await app.get_chat_member(chat_id, user_id)
+    except Exception:
+        return []
+    if member.can_post_messages:
+        perms.append("can_post_messages")
+    if member.can_edit_messages:
+        perms.append("can_edit_messages")
+    if member.can_delete_messages:
+        perms.append("can_delete_messages")
+    if member.can_restrict_members:
+        perms.append("can_restrict_members")
+    if member.can_promote_members:
+        perms.append("can_promote_members")
+    if member.can_change_info:
+        perms.append("can_change_info")
+    if member.can_invite_users:
+        perms.append("can_invite_users")
+    if member.can_pin_messages:
+        perms.append("can_pin_messages")
+    if member.can_manage_voice_chats:
+        perms.append("can_manage_voice_chats")
+    return perms
+
+
+from wbb.core.decorators.permissions import adminsOnly
 
 @run_async
 @connection_status
