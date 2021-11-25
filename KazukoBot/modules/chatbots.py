7e49eb79b2808ad92da924aedea970a6c6afbae6
おい aaman <heyaaman2004@gmail.com>
2021-11-25 13:14:26 +0530

    Update chatbots.py

--- KazukoBot/modules/chatbots.py
+++ KazukoBot/modules/chatbots.py
@@ -2,7 +2,6 @@ __help__= f"""
 
 *Kazuko AI Chatbot* 
 /addchat : Enables and Disables kazuko AI Chat mode (EXCLUSIVE)
-*Powered by* : [Kuki Chatbot](https://github.com/MoeZilla/KukiChatbot)
 """
 
 __mod_name__ = "ChatBot"
