142a6ec6aa7402a8b8f5eaa334a1bb7a737f3db2
おい aaman <heyaaman2004@gmail.com>
2021-11-25 14:05:06 +0530

    Update welcome_sql.py

--- KazukoBot/modules/sql/welcome_sql.py
+++ KazukoBot/modules/sql/welcome_sql.py
@@ -35,7 +35,7 @@ DEFAULT_WELCOME_MESSAGES = [
     "{first} just showed up. Hold my beer.",
     "Challenger approaching! {first} has appeared!",
     "It's a bird! It's a plane! Nevermind, it's just {first}.",
-    "It's {first}! Praise the sun! \o/",
+    r"It's {first}! Praise the sun! \o/",
     "Never gonna give {first} up. Never gonna let {first} down.",
     "Ha! {first} has joined! You activated my trap card!",
     "Hey! Listen! {first} has joined!",
@@ -47,28 +47,23 @@ DEFAULT_WELCOME_MESSAGES = [
     "{first} has arrived. Party's over.",
     "{first} is here to kick butt and chew bubblegum. And {first} is all out of gum.",
     "Hello. Is it {first} you're looking for?",
-    "{first} has joined. Stay awhile and listen!",
+    "{first} has joined. Stay a while and listen!",
     "Roses are red, violets are blue, {first} joined this chat with you",
-    "Welcome {first}, Avoid Punches if you can!",
     "It's a bird! It's a plane! - Nope, its {first}!",
     "{first} Joined! - Ok.",  # Discord welcome messages end.
     "All Hail {first}!",
-    "Hi, {first}. Don't lurk, only Villans do that.",
+    "Hi, {first}. Don't lurk, Only Villans do that.",
     "{first} has joined the battle bus.",
     "A new Challenger enters!",  # Tekken
     "Ok!",
     "{first} just fell into the chat!",
     "Something just fell from the sky! - oh, its {first}.",
     "{first} Just teleported into the chat!",
-    "Hi, {first}, show me your Hunter License!",  # Hunter Hunter
-    "I'm looking for Garo, oh wait nvm it's {first}.",  # One Punch man s2
-    "Welcome {first}, leaving is not an option!",
+    "Hi, {first}, show me your Hunter License!",
+    "Welcome {first}, Leaving is not an option!",
     "Run Forest! ..I mean...{first}.",
-    "{first} do 100 push-ups, 100 sit-ups, 100 squats, and 10km running EVERY SINGLE DAY!!!",  # One Punch ma
-    "Huh?\nDid someone with a disaster level just join?\nOh wait, it's just {first}.",  # One Punch ma
-    "Hey, {first}, ever heard the King Engine?",  # One Punch ma
-    "Hey, {first}, empty your pockets.",
-    "Hey, {first}!, are you strong?",
+    "Hey, {first}, Empty your pockets.",
+    "Hey, {first}!, Are you strong?",
     "Call the Avengers! - {first} just joined the chat.",
     "{first} joined. You must construct additional pylons.",
     "Ermagherd. {first} is here.",
@@ -79,19 +74,18 @@ DEFAULT_WELCOME_MESSAGES = [
     "Welcome you are",
     "Welcome {first}, your princess is in another castle.",
     "Hi {first}, welcome to the dark side.",
-    "Hola {first}, beware of people with disaster levels",
+    "Hola {first}, beware of people with nation levels",
     "Hey {first}, we have the droids you are looking for.",
     "Hi {first}\nThis isn't a strange place, this is my home, it's the people who are strange.",
     "Oh, hey {first} what's the password?",
     "Hey {first}, I know what we're gonna do today",
     "{first} just joined, be at alert they could be a spy.",
     "{first} joined the group, read by Mark Zuckerberg, CIA and 35 others.",
-    "Welcome {first}, watch out for falling monkeys.",
+    "Welcome {first}, Watch out for falling monkeys.",
     "Everyone stop what you’re doing, We are now in the presence of {first}.",
-    "Hey {first}, do you wanna know how I got these scars?",
+    "Hey {first}, Do you wanna know how I got these scars?",
     "Welcome {first}, drop your weapons and proceed to the spy scanner.",
     "Stay safe {first}, Keep 3 meters social distances between your messages.",  # Corona memes lmao
-    "Hey {first}, Do you know I once One-punched a meteorite?",
     "You’re here now {first}, Resistance is futile",
     "{first} just arrived, the force is strong with this one.",
     "{first} just joined on president’s orders.",
@@ -101,42 +95,34 @@ DEFAULT_WELCOME_MESSAGES = [
     "{first}, I have a feeling we’re not in Kansas anymore.",
     "They may take our lives, but they’ll never take our {first}.",
     "Coast is clear! You can come out guys, it’s just {first}.",
-    "Welcome {first}, pay no attention to that guy lurking.",
-    "Welcome {first}, may the force be with you.",
+    "Welcome {first}, Pay no attention to that guy lurking.",
+    "Welcome {first}, May the force be with you.",
     "May the {first} be with you.",
-    "{first} just joined. Hey, where's Perry?",
+    "{first} just joined.Hey, where's Perry?",
     "{first} just joined. Oh, there you are, Perry.",
     "Ladies and gentlemen, I give you ...  {first}.",
     "Behold my new evil scheme, the {first}-Inator.",
     "Ah, {first} the Platypus, you're just in time... to be trapped.",
+    "*snaps fingers and teleports {first} here*",
     "{first} just arrived. Diable Jamble!",  # One Piece Sanji
     "{first} just arrived. Aschente!",  # No Game No Life
     "{first} say Aschente to swear by the pledges.",  # No Game No Life
-    "{first} just joined. El Psy congroo!",  # Steins Gate
+    "{first} just joined. El psy congroo!",  # Steins Gate
     "Irasshaimase {first}!",  # weeabo shit
-    "Hi {first}, what is 1000-7?",  # tokyo ghoul
+    "Hi {first}, What is 1000-7?",  # tokyo ghoul
     "Come. I don't want to destroy this place",  # hunter x hunter
     "I... am... Whitebeard!...wait..wrong anime.",  # one Piece
     "Hey {first}...have you ever heard these words?",  # BNHA
     "Can't a guy get a little sleep around here?",  # Kamina Falls – Gurren Lagann
     "It's time someone put you in your place, {first}.",  # Hellsing
     "Unit-01's reactivated..",  # Neon Genesis: Evangelion
-    "Prepare for trouble...And make it double",  # Pokemon
-    "Hey {first}, are You Challenging Me?",  # Shaggy
+    "Prepare for trouble....And make it double",  # Pokemon
+    "Hey {first}, Are You Challenging Me?",  # Shaggy
     "Oh? You're Approaching Me?",  # jojo
-    "Ho… mukatta kuruno ka?",  # jojo jap ver
-    "I can't beat the shit out of you without getting closer",  # jojo
-    "Ho ho! Then come as close as you'd like.",  # jojo
-    "Hoho! Dewa juubun chikazukanai youi",  # jojo jap ver
-    "Guess who survived his time in Hell, {first}.",  # jojo
-    "How many loaves of bread have you eaten in your lifetime?",  # jojo
-    "What did you say? Depending on your answer, I may have to kick your ass!",  # jojo
-    "Oh? You're approaching me? Instead of running away, you come right to me? Even though your grandfather, Joseph, told you the secret of The World, like an exam student scrambling to finish the problems on an exam until the last moments before the chime?",  # jojo
-    "Rerorerorerorerorero.",  # jojo
     "{first} just warped into the group!",
     "I..it's..it's just {first}.",
     "Sugoi, Dekai. {first} Joined!",
-    "{first}, do you know gods of death love apples?",  # Death Note owo
+    "{first}, do you know Gods of death love apples?",  # Death Note owo
     "I'll take a potato chip.... and eat it",  # Death Note owo
     "Oshiete oshiete yo sono shikumi wo!",  # Tokyo Ghoul
     "Kaizoku ou ni...nvm wrong anime.",  # op
@@ -144,21 +130,20 @@ DEFAULT_WELCOME_MESSAGES = [
     "Omae wa mou....shindeiru",
     "Hey {first}, the leaf village lotus blooms twice!",  # Naruto stuff begins from here
     "{first} Joined! Omote renge!",
+    "{first} joined!, Gate of Opening...open!",
+    "{first} joined!, Gate of Healing...open!",
+    "{first} joined!, Gate of Life...open!",
+    "{first} joined!, Gate of Pain...open!",
+    "{first} joined!, Gate of Limit...open!",
+    "{first} joined!, Gate of View...open!",
+    "{first} joined!, Gate of Shock...open!",
+    "{first} joined!, Gate of Death...open!",
     "{first}! I, Madara! declare you the strongest",
     "{first}, this time I'll lend you my power. ",  # Kyuubi to naruto
     "{first}, welcome to the hidden leaf village!",  # Naruto thingies end here
-    "In the jungle, you must wait...until the dice read five or eight.",  # Jumanji stuff
+    "In the jungle you must wait...until the dice read five or eight.",  # Jumanji stuff
     "Dr.{first} Famed archeologist and international explorer,\nWelcome to Jumanji!\nJumanji's Fate is up to you now.",
-    "{first}, this will not be an easy mission - monkeys slow the expedition.",  # End of Jumanji stuff
-    "Remember, remember, the Fifth of November, the Gunpowder Treason and Plot. I know of no reason why the Gunpowder Treason should ever be forgot.", #V for Vendetta
-    "The only verdict is vengeance; a vendetta, held as a votive not in vain, for the value and veracity of such shall one day vindicate the vigilant and the virtuous.", #V for Vendetta
-    "Behind {first} there is more than just flesh. Beneath this user there is an idea... and ideas are bulletproof.", #V for Vendetta
-    "Love your rage, not your cage.", #V for Vendetta
-    "Get your stinking paws off me, you damned dirty ape!", #Planet of the apes
-    "Elementary, my dear {first}.",
-    "I'm back - {first}.",
-    "Bond. {first} Bond.",
-    "Come with me if you want to live",
+    "{first}, this will not be an easy mission - monkeys slow the expedition.",  # End of jumanji stuff
 ]
 DEFAULT_GOODBYE_MESSAGES = [
     "{first} will be missed.",
@@ -175,7 +160,7 @@ DEFAULT_GOODBYE_MESSAGES = [
     "Goodbye {first}! Guess who's gonna miss you :')",
     "Goodbye {first}! It's gonna be lonely without ya.",
     "Please don't leave me alone in this place, {first}!",
-    "Good luck finding better shit-posters than us, {first}!",
+    "Good luck finding better shitposters than us, {first}!",
     "You know we're gonna miss you {first}. Right? Right? Right?",
     "Congratulations, {first}! You're officially free of this mess.",
     "{first}. You were an opponent worth fighting.",
@@ -204,8 +189,8 @@ DEFAULT_GOODBYE_MESSAGES = [
     "Trust good people",
     "Live to die.",
     "When life gives you lemons reroll!",
-    "Well, that was worthless",
-    "I fell asleep!",
+    "Well that was worthless",
+    "I feel asleep!",
     "May your troubles be many",
     "Your old life lies in ruin",
     "Always look on the bright side",
@@ -222,6 +207,8 @@ DEFAULT_GOODBYE_MESSAGES = [
     "Go outside",
     "Always your head in the clouds",
 ]
+
+
 # Line 111 to 152 are references from https://bindingofisaac.fandom.com/wiki/Fortune_Telling_Machine
 
 
@@ -233,7 +220,7 @@ class Welcome(BASE):
     custom_content = Column(UnicodeText, default=None)
 
     custom_welcome = Column(
-        UnicodeText, default=random.choice(DEFAULT_WELCOME_MESSAGES),
+        UnicodeText, default=random.choice(DEFAULT_WELCOME_MESSAGES)
     )
     welcome_type = Column(Integer, default=Types.TEXT.value)
 
@@ -249,7 +236,7 @@ class Welcome(BASE):
 
     def __repr__(self):
         return "<Chat {} should Welcome new users: {}>".format(
-            self.chat_id, self.should_welcome,
+            self.chat_id, self.should_welcome
         )
 
 
@@ -397,10 +384,8 @@ def get_welc_pref(chat_id):
             welc.custom_content,
             welc.welcome_type,
         )
-
-    else:
-        # Welcome by default.
-        return True, DEFAULT_WELCOME, None, Types.TEXT
+    # Welcome by default.
+    return True, DEFAULT_WELCOME, None, Types.TEXT
 
 
 def get_gdbye_pref(chat_id):
@@ -408,9 +393,8 @@ def get_gdbye_pref(chat_id):
     SESSION.close()
     if welc:
         return welc.should_goodbye, welc.custom_leave, welc.leave_type
-    else:
-        # Welcome by default.
-        return True, DEFAULT_GOODBYE, Types.TEXT
+    # Welcome by default.
+    return True, DEFAULT_GOODBYE, Types.TEXT
 
 
 def set_clean_welcome(chat_id, clean_welcome):
@@ -460,7 +444,7 @@ def set_gdbye_preference(chat_id, should_goodbye):
 
 
 def set_custom_welcome(
-    chat_id, custom_content, custom_welcome, welcome_type, buttons=None,
+    chat_id, custom_content, custom_welcome, welcome_type, buttons=None
 ):
     if buttons is None:
         buttons = []
