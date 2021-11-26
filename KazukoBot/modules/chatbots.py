f2474d0fe7e2227294f82b3be9e75daa1f72f92e
Ashik Muhammed <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-11-26 18:33:44 +0530

    🔂

--- KazukoBot/modules/chatbots.py
+++ KazukoBot/modules/chatbots.py
@@ -1,7 +1,8 @@
 __help__= f"""
 
-*Kazuko AI Chatbot* 
-/addchat : Enables and Disables kazuko AI Chat mode (EXCLUSIVE)
+<b> Kazuko Chatbot </b>
+/addchat : Enables and Disables kazuko AI Chat mode on Group and PM.(EXCLUSIVE)
+Powered By [kazuko](https://t.me/KazukoRobot) And Kuki. 
 """
 
 __mod_name__ = "ChatBot"
