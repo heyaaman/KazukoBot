8ce347da98cce2e80f5b1a8113d2c683e2e713af
Aaman <heyaaman2004@gmail.com>
2022-03-07 19:06:07 +0530

    Update logo.py

--- KazukoBot/modules/logo.py
+++ KazukoBot/modules/logo.py
@@ -152,7 +152,7 @@ file_helpo = file_help.replace("_", " ")
 
 
 __help__ = """
- ❍ /logo text :  Create your logo with your name
- ❍ /wlogo text :  Create your logo with your name
+ • /logo text :  Create your logo with your name
+ • /wlogo text :  Create your logo with your name
  """
 __mod_name__ = "Logo"
