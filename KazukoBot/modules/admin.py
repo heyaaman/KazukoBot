6a0210ce0f2ac639ca211c1c27471fb52f44e25b
Aaman <heyaaman2004@gmail.com>
2022-03-07 18:59:44 +0530

    Update admin.py

--- KazukoBot/modules/admin.py
+++ KazukoBot/modules/admin.py
@@ -742,28 +742,28 @@ def adminlist(update, context):
 
 
 __help__ = """
- ❍ /admins*:* list of admins in the chat
+ • /admins*:* list of admins in the chat
 *Admins only:*
- ❍ /pin*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
- ❍ /unpin*:* unpins the currently pinned message
- ❍ /invitelink*:* gets invitelink
- ❍ /promote*:* promotes the user
- ❍ /fullpromote <title>: Promotes the user replied to with ful rights.
- ❍ /demote*:* demotes the user
- ❍ /title <title here>*:* sets a custom title for an admin that the bot promoted
- ❍ /setgtitle <newtitle>*:* Sets new chat title in your group.
- ❍ /setgpic*:* As a reply to file or photo to set group profile pic!
- ❍ /delgpic*:* Same as above but to remove group profile pic.
- ❍ /setsticker*:* As a reply to some sticker to set it as group sticker set!
- ❍ /setdescription <description>*:* Sets new chat description in group.
- ❍ /admincache*:* force refresh the admins list
- ❍ /antispam <on/off/yes/no>*:* Will toggle our antispam tech or return your current settings.
- ❍ /del*:* deletes the message you replied to
- ❍ /purge*:* deletes all messages between this and the replied to message.
- ❍ /purge <integer X>*:* deletes the replied message, and X messages following it if replied to a message.
- ❍ /rules*:* get the rules for this chat.
- ❍ /setrules <your rules here>*:* set the rules for this chat.
- ❍ /clearrules*:* clear the rules for this chat.
+ • /pin*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
+ • /unpin*:* unpins the currently pinned message
+ • /invitelink*:* gets invitelink
+ • /promote*:* promotes the user
+ • /fullpromote <title>: Promotes the user replied to with ful rights.
+ • /demote*:* demotes the user
+ • /title <title here>*:* sets a custom title for an admin that the bot promoted
+ • /setgtitle <newtitle>*:* Sets new chat title in your group.
+ • /setgpic*:* As a reply to file or photo to set group profile pic!
+ • /delgpic*:* Same as above but to remove group profile pic.
+ • /setsticker*:* As a reply to some sticker to set it as group sticker set!
+ • /setdescription <description>*:* Sets new chat description in group.
+ • /admincache*:* force refresh the admins list
+ • /antispam <on/off/yes/no>*:* Will toggle our antispam tech or return your current settings.
+ • /del*:* deletes the message you replied to
+ • /purge*:* deletes all messages between this and the replied to message.
+ • /purge <integer X>*:* deletes the replied message, and X messages following it if replied to a message.
+ • /rules*:* get the rules for this chat.
+ • /setrules <your rules here>*:* set the rules for this chat.
+ • /clearrules*:* clear the rules for this chat.
 
 *Note:* Night Mode chats get Automatically closed at 12 am(IST)
 and Automatically openned at 6 am(IST) To Prevent Night Spams.
