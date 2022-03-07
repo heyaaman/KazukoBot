ff634da4d054ea7909525df688aa2195116d2ae7
Aaman <heyaaman2004@gmail.com>
2022-03-07 21:16:34 +0530

    Update __init__.py

--- KazukoBot/modules/sql/__init__.py
+++ KazukoBot/modules/sql/__init__.py
@@ -1,15 +1,23 @@
-from KazukoBot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+from KazukoBot import DB_URI, log
 
 
 def start() -> scoped_session:
     engine = create_engine(DB_URI, client_encoding="utf8")
+    log.info("[PostgreSQL] Connecting to database......")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
 BASE = declarative_base()
-SESSION = start()
+try:
+    SESSION: scoped_session = start()
+except Exception as e:
+    log.exception(f'[PostgreSQL] Failed to connect due to {e}')
+    exit()
+   
+log.info("[PostgreSQL] Connection successful, session started.")
