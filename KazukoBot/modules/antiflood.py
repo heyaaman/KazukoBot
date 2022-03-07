878b0d92326e6e1473b125e0eccf19e5dfbc5d79
Aaman <heyaaman2004@gmail.com>
2022-03-07 18:45:04 +0530

    Update antiflood.py

--- KazukoBot/modules/antiflood.py
+++ KazukoBot/modules/antiflood.py
@@ -418,7 +418,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "Anti_Flood"
+__mod_name__ = "Anti-Flood"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood,
